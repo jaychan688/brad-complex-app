1869c7a57a2d3a1d9f376f98993581b271acff41
Jay Chan <jaychan688@gmail.com>
2021-08-15 16:59:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# brad-complex-app
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const router = require('./router')
+
+// It serves static files and is based on serve-static.
+app.use(express.static('public'))
+// A directory or an array of directories for the application's views.
+app.set('views', 'views')
+// Set template engine
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+
+app.listen(3000)
